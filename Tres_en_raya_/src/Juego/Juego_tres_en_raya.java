56396975f4c693a0eb0a3363c8323532f925f42a
PolPlaza <polplaza44@gmail.com>
2024-12-12 18:52:31 +0100

    Prueba

---
+++ Tres_en_raya_/src/Juego/Juego_tres_en_raya.java
@@ -0,0 +1,11 @@
+package Juego;
+
+public class Juego_tres_en_raya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Prueba
+	}
+
+}
---
+++ Tres_en_raya_/src/module-info.java
@@ -0,0 +1,2 @@
+module Tres_en_raya_ {
+}
